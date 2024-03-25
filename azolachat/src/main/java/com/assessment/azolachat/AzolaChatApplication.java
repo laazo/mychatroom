e7007113dda3b2f001c0ac3d9cb930f4bb833ce5
Azola <azola17@gmail.com>
2024-03-25 11:15:35 +0200

    Authour initial commit

---
+++ azolachat/src/main/java/com/assessment/azolachat/AzolaChatApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.azolachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzolaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzolaChatApplication.class, args);
+	}
+
+}
---
+++ azolachat/src/test/java/com/assessment/azolachat/AzolachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.azolachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzolachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
