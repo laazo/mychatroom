8989f2e2f9da2d650526e188681ecfb46e9bcd9f
Azola <azola17@gmail.com>
2024-03-26 08:45:57 +0200

    Add request mapping, add missing request body annotations

--- azolachat/src/main/java/com/assessment/azolachat/api/controller/ChatRoomController.java
+++ azolachat/src/main/java/com/assessment/azolachat/api/controller/ChatRoomController.java
@@ -9,25 +9,28 @@ import com.assessment.azolachat.exception.ChatRoomException;
 import com.assessment.azolachat.service.ChatRoomService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.assessment.azolachat.api.util.ModelMapper.buildResponse;
 
 @RestController
+@RequestMapping("chat")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
     public ChatRoomController(ChatRoomService chatRoomService) {
         this.chatRoomService = chatRoomService;
     }
 
-    @PostMapping(path = "chat/create-chatroom", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public CreateChatRoomResponse createChatRoom(CreateChatRoomRequest createChatRoomRequest) throws ChatRoomException {
+    @PostMapping(path = "/create-chatroom", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public CreateChatRoomResponse createChatRoom(@RequestBody CreateChatRoomRequest createChatRoomRequest) throws ChatRoomException {
         var chatRoom = chatRoomService.createChatRoom(createChatRoomRequest.getChatRoomName());
         return buildResponse(chatRoom);
     }
 
-    @PostMapping(path = "chat/send-message", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SendMessageResponse sendMessage(SendMessageRequest sendMessageRequest) throws ChatRoomException {
+    @PostMapping(path = "/send-message", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public SendMessageResponse sendMessage(@RequestBody SendMessageRequest sendMessageRequest) throws ChatRoomException {
         var message = chatRoomService.sendMessage(sendMessageRequest.getUserId(), sendMessageRequest.getChatRoomId(), sendMessageRequest.getMessageContent());
         return buildResponse(message);
     }
